40cafd6d553f9ea962b12f5c577a101f44dfcd1b
Oluwafemi Korede lawrence <o.korede@exolvemedia.com>
2014-04-03 09:25:58 +0100

    5th commit
    yes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-function showAlert(message, title) {
+var app = {
+    showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
     }
-
+};
